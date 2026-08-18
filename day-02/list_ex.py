26630b957c415b0afdd4a882ad47d7e100af942c
vipulraghupatruni <vipulraghupatrunni5136@gmail.com>
2026-08-19 00:08:56 +0530

    python for devops

--- day-02/list_ex.py
+++ day-02/list_ex.py
@@ -13,3 +13,18 @@ clouds.append("ibm")
 clouds.append("oracle")
 
 print(clouds)
+
+print("length of list is : ",len(clouds))
+print("cloud service provider is : ",clouds[0])
+
+print("cloud service provider is : ",clouds[-1])   #last
+print("cloud service provider is : ",clouds[-2])     #second last
+
+
+
+print(dir(clouds))  #list of all methods in list
+print(clouds.count.__doc__)  #Return number of occurrences of value.
+print(clouds.reverse.__doc__) #Reverse *IN PLACE*
+print(clouds.append.__doc__)  #Append object to the end of the list.
+print(clouds.extend.__doc__)  #Extend list by appending elements from the iterable.
+
