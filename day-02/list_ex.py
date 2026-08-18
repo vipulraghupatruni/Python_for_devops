00267ee64aff2feae400b9ffd8e57edbb7074a0a
vipulraghupatruni <vipulraghupatrunni5136@gmail.com>
2026-08-18 23:54:09 +0530

    python for devops

---
+++ day-02/list_ex.py
@@ -0,0 +1,15 @@
+a = [100,200,300,400, True, 4.6]
+print(type(a))
+a.append(500)
+print(a)
+
+clouds = list() #list
+print(type(clouds))
+
+clouds.append("aws")
+clouds.append("azure")
+clouds.append("gcp")
+clouds.append("ibm")
+clouds.append("oracle")
+
+print(clouds)
